baa1b7bedfcd02db640ee888a44a30f2e64f4bb4
Harish <hareeshseen@gmail.com>
2025-01-24 10:22:53 +0530

    typescript variables

---
+++ typescript/variables.ts
@@ -0,0 +1,22 @@
+// variables.ts
+
+/* 
+string
+Partial<T>
+Union (T | U)
+ReadonlyArray<T>
+Date
+Literal types (e.g., 42, "hello")
+Iterator<T>
+Pick<T, K>
+Generator<T, TReturn, TNext>
+Abstract classes
+*/
+
+console.log("-----------------------------------------------------------");
+
+const str: string = "harish";
+console.log("String:",str);
+
+console.log("-----------------------------------------------------------");
+
